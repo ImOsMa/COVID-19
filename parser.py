82bc3d6c8674280892036f9d6328d6ec01460939
ImOsMa <30871788+ImOsMa@users.noreply.github.com>
2020-04-16 01:47:48 +0300

    Нашли median_age
    Распарсили с помощью BeautifulSoup средний возраст в разных штатах

--- parser.py
+++ parser.py
@@ -3,8 +3,45 @@ import urllib.request
 from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
+""""
+def get_html(url,   params = None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('table', class_='datatableStyles__StyledTable-bwtkle-1 hOnuWY table table-striped')
+    #items = soup.find_all('a', href=True)
+    #items = soup.find_all('a', href=True)
+    print(items)
+    country = []
+    for item in items:
+        pass
+def parse():
+    html = get_html(URL)
+    if (html.status_code) == 200: # status_code делает более читабельный формат
+        print('OK')
+        get_content(html.text)
+    else:
+        print('Error')
 
-#HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36', 'accept':'*/*'} # для заголовков, будем имитировать работу браузера
+parse()
+class WikiParser(object):
+
+    def parse(self):
+        self.go_to_ourtable()
+    def go_to_ourtable(self):
+        self.driver.get("https://en.wikipedia.org/wiki/List_of_countries_by_median_age")
+
+def main():
+    driver = webdriver.Chrome()
+    driver.get("https://en.wikipedia.org/wiki/List_of_countries_by_median_age")
+    #btn_elem = driver.find_element_by_class_name("/wiki/Afghanistan")
+    #btn_elem.click()
+    print(name.text)
+if __name__ == "__main__":
+    main()
+"""
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36', 'accept':'*/*'} # для заголовков, будем имитировать работу браузера
 URL = 'https://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_median_age'
 
 """
@@ -17,27 +54,25 @@ for c in l:
     states.append(c.get('title'))
 l = my_table.findAll()
 """
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, 'html.parser')
+table = soup.find('table', {'class':'wikitable sortable'}).tbody
 
+rows = table.find_all('tr')
 
-article = open('site.html').read()
-soup = BeautifulSoup(article, 'html.parser')
-my_table = soup.findAll('table', class_='wikitable sortable')
+columns = [v.text.replace('\n', '').replace('[6]', '') for v in rows[0].find_all('th')]
+#print(columns)
+df = pd.DataFrame(columns = columns)
 
-for table in my_table:
-    ths = table.findAll('th')
-    headings = [th.text.strip() for th in ths]
-    if headings[:2] == ['State,'
-                        'federal district,'
-                        'or territory','Median age'
-                                       'in years'
-                                       '(Total'
-                                       'Population)']:
-        break
+for i in range(1, len(rows)):
+    tds = rows[i].find_all('td')
 
-with open ('data.txt', 'w') as fo:
-    for tr in my_table.findAll('tr'):
-        tds = tr.findAll('td')
-        if not tds:
-            continue
-        state, age = [td.text.strip() for td in tds[:2]]
-        print(', '.join([state,age]), file=fo)
+    if len(tds) == 3:
+        values = [tds[0].text.replace('\n', ''), tds[1].text.replace('\n','').replace('\xa0','').replace('[7]',''), tds[2].text.replace('\n','')]
+    else:
+        values = [td.text.replace('\n','').replace('\xa0','').replace('[7]','') for td in tds]
+    print(values)
+
+    df = df.append(pd.Series(values, index=columns), ignore_index = True)
+
+    df.to_csv('median_age.csv', index=False)
