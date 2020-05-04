7d87bee5d8dadf8de19785fd604149f0b367a769
ImOsMa <30871788+ImOsMa@users.noreply.github.com>
2020-05-04 23:48:48 +0300

    Update parser4(quarantine).py

--- parser4(quarantine).py
+++ parser4(quarantine).py
@@ -16,19 +16,33 @@ columnsDown = [v.text.replace('\n', '') for v in rows[1].find_all('th')]
 columnsUp.remove('Closures ordered')
 columnsUp.remove('Sources')
 columns = columnsUp + columnsDown
-#print(columnsUp)
+columns.remove('State/territory')
+columns.append('Sources')
+print(columnsUp)
 #print(columnsDown)
 print(columns)
 df = pd.DataFrame(columns = columns)
+val = []
 
 for i in range(1, len(rows)):
     tds = rows[i].find_all('td')
-
+    #print(rows[i])
+    #print(len(tds))
+    #print(len(columns))
     if len(tds) == len(columns):
-        values = [tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text, tds[5].text, tds[6].text, tds[7].text, tds[8].text]
+        values = [tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text, tds[5].text, tds[6].text, tds[7].text,tds[8].text]
 
     else:
         values = [td.text for td in tds]
-    print(values)
-    if values:
-        df = df.append(pd.Series(values, index = columns), ignore_index=True)
+    #print(values)
+    if len(tds) > 0:
+        df = df.append(pd.Series(values, index = columns), ignore_index = True)
+for i in range(2, len(rows)):
+    tds = rows[i].find_all('a')
+    #print(tds, " - ", i)
+    title = tds[0].get('title')
+    val.append(title)
+print(val)
+df.drop('Sources', axis=1, inplace=True)
+df['State/territory'] = val
+print(df)
